c47d55abebbe390f26f94aa767a927a6eea1ea8d
jeremy <jereemy.garcia@gmail.com>
2025-06-18 21:57:29 +0200

    correc

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pages/p2308905/portfolio-jeremy/',
+  base: '/portfolio/',
   plugins: [react()],
 })
